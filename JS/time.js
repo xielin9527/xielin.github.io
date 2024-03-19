5b7cf09016c6132f92d6ccbf137b9f50a800a68c
xielin <xielin_9527@qq.com>
2024-03-19 15:45:47 +0800

    feat: 样式调整

--- JS/time.js
+++ JS/time.js
@@ -1,2 +1,9 @@
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleString();
+function updateClock() {
+  var now = new Date();
+  document.getElementById("datetime").innerHTML = now.toLocaleString();
+  // 使用 requestAnimationFrame 递归调用，以保证每次重绘前都更新时钟
+  requestAnimationFrame(updateClock);
+}
+
+// 页面加载时立即更新时钟
+updateClock()
