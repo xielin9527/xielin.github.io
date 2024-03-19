1b053c8e68195722f4f85c2e4220117f492d74a1
xielin <xielin_9527@qq.com>
2024-03-19 11:38:38 +0800

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service worker activated');
+});
